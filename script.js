9c5fd122fd7b8836cd762456fa123421267f18fc
Balaji <155710213+Balaji19072007@users.noreply.github.com>
2024-01-06 16:07:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,26 +17,3 @@ downBtn.onclick = function()
     circle.style.transform = rotateSum;
     rotateValue = rotateSum;
 }
-const slides = document.querySelectorAll(".slider")
-var counter = 0;
-slides.forEach(
-    (slider,index) => {
-        slider.style.left = `${index * 100}%`
-    }
-)
-const goPrev = () => {
-    counter--
-    slideImage()
-}
-const goNext = () => {
-    counter++
-    slideImage()
-}
-
-const slideImage = () => {
-    slides.forEach(
-        (slider) => {
-            slider.style.transform = `translateX(-${counter * 100}%)`
-        }
-    )
-}
